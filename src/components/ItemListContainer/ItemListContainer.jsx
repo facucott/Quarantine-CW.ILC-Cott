bf248c61de07fed799ccfbeb5261ed2223c2ea90
Facu Cott <facumcott@gmail.com>
2022-08-28 22:27:50 -0300

    props

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,28 +1,9 @@
-export default function ItemListContainer (){
+export default function ItemListContainer (props){
     return(
-        <div className="titulo">
-            <br />
-        <h2>¡Bienvenidos al sitio oficial de The Three Of Quarantine!</h2>
-        <br />
-        <p>
-            <i>
-                "... rock crudo y de alma góspel: interpretaciones minimalistas que dejan entreoír un lejano traqueteo, crujen en estrépito y se silencian..."
-            </i>
-        </p>
-        <br />
-        <hr />
-        <br />
-        <p>
-            <h5>
-            The Three of Quarantine nace durante la - ya conocida - pandemia, como la puesta en marcha de un viejo proyecto de tres amigos: 
-            </h5>
-            <br /><br /><br />
-            <h6>
-            <strong>Gabriel Casiraghi</strong> (Voz/Guitarra Acústica), <br /><br />
-            <strong>Juan Comi</strong> (Cigarbox/Guitarra Eléctrica/Coros), <br /><br />
-            <strong>Facundo Cott</strong> (Washboard/Batería/Coros).
-            </h6>
-        </p>
+        <div className="musicos">
+        <h6>
+            <strong>{props.name}</strong> <br /> <br /> ({props.instrument})
+        </h6>
         </div>
     )
 }
